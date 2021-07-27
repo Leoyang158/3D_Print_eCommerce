937f247259f63d6236f04107ab6e4dd835f84531
Leohawk <Leoyang158@gmail.com>
2021-07-27 00:21:11 -0500

    Saved

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1> Welcome to 3D Print Shop</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
