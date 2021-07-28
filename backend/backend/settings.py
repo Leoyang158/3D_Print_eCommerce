7224ae96379c3059b5ede0084940badfa2e3e11c
Leohawk <Leoyang158@gmail.com>
2021-07-28 02:17:34 -0500

    Update the dataBase

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,10 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'corsheaders',
     'base.apps.BaseConfig',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,6 +80,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Connetion for the dataBase
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -125,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,5 +3,7 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name='routes')
+    path('', views.getRoutes, name='routes'),
+    path('products/', views.getProducts, name='products'),
+    path('products/<str:pk>/', views.getProduct, name='product')
 ]
