47ddc27a3776965045e8dd54083d2ff3af958176
Leohawk <Leoyang158@gmail.com>
2021-07-28 17:33:56 -0500

    Create backend setting for auth and frontend shopping cart (UX)

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,8 @@
 import {
     CART_ADD_ITEM,
-    // CART_REMOVE_ITEM,
+
+    CART_REMOVE_ITEM,
+
     // CART_SAVE_SHIPPING_ADDRESS,
 
     // CART_SAVE_PAYMENT_METHOD,
@@ -30,11 +32,11 @@ export const cartReducer = (state = { cartItems: []}, action) => {
                 }
             }
 
-        // case CART_REMOVE_ITEM:
-        //     return {
-        //         ...state,
-        //         cartItems: state.cartItems.filter(x => x.product !== action.payload)
-        //     }
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(x => x.product !== action.payload)
+            }
 
         // case CART_SAVE_SHIPPING_ADDRESS:
         //     return {
